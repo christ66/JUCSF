cf080eecb87b3309b05c70fb421c23bd17cf538b
christ66 <schristou88@gmail.com>
2014-10-23 17:19:40 -0700

    Add Beerbuilder

--- src/main/java/org/jenkinsci/plugins/JUCSF/BeerBuilder.java
+++ src/main/java/org/jenkinsci/plugins/JUCSF/BeerBuilder.java
@@ -4,6 +4,8 @@ import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
+import hudson.model.FreeStyleProject;
+import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -21,5 +23,20 @@ public class BeerBuilder extends Builder {
 PrintStream console = listener.getLogger();
 console.println("hello JUC");
 return true;
-    } 
+    }
+
+    @Extension
+    public static class DescriptorImpl
+            extends BuildStepDescriptor<Builder> {
+
+        @Override
+        public boolean isApplicable(Class aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Beer builder";
+        }
+    }
 }
